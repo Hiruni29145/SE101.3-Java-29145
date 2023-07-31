159047b27943908a7d7e16b66d5e8b8d019f1e1b
Hiruni29145 <137154585+Hiruni29145@users.noreply.github.com>
2023-07-31 21:31:24 +0530

    Add files via upload

---
+++ Java Lab5 Q1,3,4/Practical5qu3/src/main/java/com/mycompany/practical5qu3/Practical5qu3.java
@@ -0,0 +1,10 @@
+
+package com.mycompany.practical5qu3;
+
+public class Practical5qu3 {
+
+    public static void main(String[] args) 
+    {
+     
+    }
+}
---
+++ Java Lab5 Q1,3,4/Practical5qu4/src/main/java/com/mycompany/practical5qu4/Practical5qu4.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.practical5qu4;
+
+/**
+ *
+ * @author HP
+ */
+public class Practical5qu4 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
