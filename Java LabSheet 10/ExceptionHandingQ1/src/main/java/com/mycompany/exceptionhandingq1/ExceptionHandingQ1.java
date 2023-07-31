302915284a6252f5be8b45c3ed3e1529aa7e0af5
Hiruni29145 <137154585+Hiruni29145@users.noreply.github.com>
2023-07-31 09:34:35 +0530

    Add files via upload

---
+++ Java LabSheet 10/ExceptionHandingQ1/src/main/java/com/mycompany/exceptionhandingq1/ExceptionHandingQ1.java
@@ -0,0 +1,25 @@
+
+package com.mycompany.exceptionhandingq1;
+
+import java.util.Scanner;
+public class ExceptionHandingQ1 {
+
+    public static void main(String[] args)
+    {
+        int n1,n2,ans;
+        Scanner sc= new Scanner(System.in);
+        try
+        {
+            System.out.println("Enter first number: ");
+            n1=sc.nextInt();
+            System.out.println("Enter second number: ");
+            n2=sc.nextInt();
+            ans=n1/n2;
+            System.out.println("Answer is "+ans);
+        }
+        catch(ArithmeticException e)
+        {
+            System.out.println("Divided by zero error!");
+        }
+    }
+}
